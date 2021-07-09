38303a25329efc2494e0157153af9024bb36b645
Marcella Bueno <marcellabupiresn@gmail.com>
2021-07-09 01:45:51 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] ={
     y: 8* box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 
 function criarBG() {
@@ -21,6 +25,12 @@ function criarCobrinha(){
     }
 
 }
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 
 document.addEventListener('keydown', update);
 
@@ -44,6 +54,7 @@ function inciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
